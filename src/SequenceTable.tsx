41ed3f48dded3b6ca24458bd897c541d0e998fd6
Matthew Wang <matt@matthewwang.me>
2023-02-16 23:56:57 -0800

    minor table formatting

--- src/SequenceTable.tsx
+++ src/SequenceTable.tsx
@@ -7,20 +7,20 @@ type Props = {
 
 export default function SequenceTable({k, processedSequences}: Props){
   return (
-    <table className='border-spacing-8'>
-      <thead>
+    <table className='text-left border-spacing-8 overflow-x-auto block max-w-fit'>
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
-          <th>#</th>
-          <th>sequence</th>
-          <th>{k}-mers</th>
+          <th className='px-6 py-3'>#</th>
+          <th className='px-6 py-3'>sequence</th>
+          <th className='px-6 py-3'>{k}-mers</th>
         </tr>
       </thead>
       <tbody>
         {processedSequences.map((processedSequence, i) =>
-          <tr key={processedSequence.sequence}>
-            <td>{i}</td>
-            <td>{processedSequence.sequence}</td>
-            <td className='p-2.5'>
+          <tr className='border-b dark:border-gray-700' key={processedSequence.sequence}>
+            <td className='px-6 py-3'>{i}</td>
+            <td className='px-6 py-3'>{processedSequence.sequence}</td>
+            <td className='px-6 py-3'>
               <ul>
                 {Array.from(processedSequence.kmers).map(key =>
                   <li className="inline border-2 border-blue-500 rounded-lg p-1.5 m-1.5" key={key}>{key}</li>)
